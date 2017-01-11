ac206bbef6f68c4066daa973c12aa764ebd50b5f
Shubham Singh <coolsks94@gmail.com>
2017-01-11 22:30:00 +0000

    AospExtended Bringup

--- aosp_merlin.mk
+++ aosp_merlin.mk
@@ -14,16 +14,13 @@
 
 $(call inherit-product, device/motorola/merlin/full_merlin.mk)
 
-# Inherit CM common stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := cm_merlin
+PRODUCT_NAME := aosp_merlin
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := merlin
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_merlin-userdebug
-add_lunch_combo cm_merlin-eng
+add_lunch_combo aosp_merlin-userdebug
+add_lunch_combo aosp_merlin-eng
